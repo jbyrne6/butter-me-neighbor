1e535da3ac8abb495a093b1235b13ee601d3e6e8
Jimmy Byrne <jbyrne5094@gmail.com>
2020-10-16 03:13:28 -0700

    made svg

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation()
+
+function alertButtered() {
+    alert('I have been buttered!')
+}
+
+window.addEventListener('mouseup', (e) => {
+    // Let's pick a random color between #000000 and #FFFFFF
+    const color = Math.round(Math.random() * 0xFFFFFF)
+  
+    // Let's format the color to fit CSS requirements
+    const fill = '#' + color.toString(16).padStart(6,'0')
+  
+    // Let's apply our color in the
+    // element we actually clicked on
+    e.target.style.fill = fill
+  })
